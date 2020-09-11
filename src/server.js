bdb9313ca9b400f67d0e8ae3cfbb9a8ccff11e22
Anoop Mundathan <anoop.mundathan@amicissolutions.com>
2020-09-11 23:35:59 +0100

    Add cors

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const fetch = require('node-fetch')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
 
 const app = express()
 
@@ -7,6 +10,10 @@ const API_URL = 'https://api.smarkets.com/v3'
 const PORT = process.env.PORT || 8000
 
 const removeQuote = (text) => text.split("'").join('')
+
+app.use(cors())
+app.use(morgan('combined'))
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
   res.json('')
@@ -31,8 +38,14 @@ app.get('/api/event-types', (req, res) => {
     .catch(() => res.status(500).send('Something went wrong'))
 })
 
-app.get('/api/events', (req, res) => {
-  fetch(`${API_URL}/events`)
+app.get('/api/events/:eventType', (req, res) => {
+  const { eventType } = req.params
+
+  if (!eventType) {
+    res.status(400).send('Event type is required')
+  }
+
+  fetch(`${API_URL}/events/?state=upcoming&type=${eventType}`)
     .then((res) => res.json())
     .then((data) => res.json(data))
     .catch(() => res.status(500).send('Something went wrong'))
